6ae2df8a54ccb8b45c540432a17ee1d9bb8f2cd6
Sergei Ananev <9852407799s@gmail.com>
2024-05-21 20:05:56 +0200

    feat: update local repo

--- project/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { State, AppDispatch } from '../types/state';
-
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
